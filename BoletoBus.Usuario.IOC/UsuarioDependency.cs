8071ed6352dc6e5fd8094749742f1bd8555d3069
Syo <diazdaybelsamuel@gmail.com>
2024-07-13 09:57:51 -0400

    first

---
+++ BoletoBus.Usuario.IOC/UsuarioDependency.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace BoletoBus.Usuario.IOC
+{
+    public class UsuarioDependency
+    { 
+        public static void AddUsuarioDependency (this IServiceCollection service)
+    }
+}
