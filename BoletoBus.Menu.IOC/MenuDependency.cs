8ab123aa86bccbf7de9387b8d78676a0f60d8e27
Syo <diazdaybelsamuel@gmail.com>
2024-07-13 11:07:51 -0400

    IOC Menu

---
+++ BoletoBus.Menu.IOC/MenuDependency.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.NetworkInformation;
+
+namespace BoletoBus.Menu.IOC
+{
+    public static class MenuDependency
+    {
+        public static void AddMenuDependency(this IserviceCollection service) 
+        { 
+        }
+    }
+}
